13b176aef9f3f8d371f226b9f73383a84cce300e
Antonio Salazar Cardozo <antonio@keep.network>
2019-02-24 22:28:19 -0500

    Add adapter parameter
    Just one option, repl, right now, and no provision for expanding this
    straightforwardly yet...

--- Sources/gordias/main.swift
+++ Sources/gordias/main.swift
@@ -5,12 +5,41 @@ import GordiasChat
 
 let arguments = Array(ProcessInfo.processInfo.arguments.dropFirst())
 
+enum Adapter: ArgumentKind {
+    init(argument: String) throws {
+        switch argument {
+        case "repl":
+            self = .repl
+        default:
+            self = .repl
+        }
+    }
+    
+    static var completion: ShellCompletion = .values([("repl", "Interact with chat via the console.")])
+    
+    case repl
+    
+    var bot: ChatBot {
+        get {
+            switch self {
+            case .repl: return ReplBot()
+            }
+        }
+    }
+}
+
 let parser = ArgumentParser(usage: "<options>", overview: "run modules in the gordias operations system")
+let adapterArgument = parser.add(option: "--adapter", shortName: "-a", kind: Adapter.self, usage: "Specify the adapter to use; defaults to repl", completion: Adapter.completion)
 
-let parsed = try parser.parse(arguments)
+do {
+    let parsed = try parser.parse(arguments)
+    let adapter = parsed.get(adapterArgument) ?? Adapter.repl
+    let bot = adapter.bot
 
-let replBot = ReplBot()
-replBot.initialize(botName: "Heimdall")
-try replBot.listen(forPattern: "^Hello", responding: { (message) in "Ohai!" })
+    bot.initialize(botName: "Heimdall")
+    try bot.listen(forPattern: "^Hello", responding: { (message) in "Ohai!" })
 
-replBot.listen()
+    bot.listen()
+} catch let ape as ArgumentParserError {
+    FileHandle.standardError.write(String(describing: ape).data(using: .utf8)!)
+}
